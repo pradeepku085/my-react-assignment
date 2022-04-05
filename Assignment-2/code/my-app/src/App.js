6941d623c3937e234aa523f529212efa8f7a385f
pradeepku085 <pradeepku.085@gmail.com>
2022-04-05 13:50:25 +0530

    assignment-2 Display Simple Object(Data) with JSX/REACT Submission

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
 
 
 function App(props) {
  const {employee}=props;
     
 return(
-  <div className="container">
+<div className="container">
   {/* Start your React code here */}
+  <img src={employee.profileImg} alt="#"/>
+  <div className="info">
+  <h1>{employee.name}</h1>
+    <p>Location</p>
+    <p><strong>{employee.location}</strong></p>
+    <p>BloodGroup</p>
+    <p><strong>{employee.bloodGroup}</strong></p>
+    <p>Age</p>
+    <p><strong>{employee.age}</strong></p>
+  </div>
 </div>
 )
 }
